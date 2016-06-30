c7f5d16f22011341ff4effef428975e55bfc6697
Jay McDaniel <jaymcdan@gmail.com>
2016-06-30 11:04:26 -0400

    'added tooltip follow for bubble plotoptions'

--- scripts_src/constructors/charts/plot_options.js
+++ scripts_src/constructors/charts/plot_options.js
@@ -1,7 +1,7 @@
 /** PlotOptions constructor. Info at http://api.highcharts.com/highcharts#plotOptions
-* @constructor PlotOptions 
-* @param o {object} PlotOptions options object
-*/
+ * @constructor PlotOptions 
+ * @param o {object} PlotOptions options object
+ */
 var PlotOptions = function (o) {
 
     //for all chart types. values are undefined if not in o.
@@ -14,15 +14,21 @@ var PlotOptions = function (o) {
         },
         pointPlacement: o.series.pointPlacement,
         stacking: o.series.stacking,
-        pointPadding: o.series.pointPadding, 
+        pointPadding: o.series.pointPadding,
         groupPadding: o.series.groupPadding,
         minPointLength: o.series.minPointLength,
         maxSize: o.series.maxSize,
-        sizeBy: o.series.sizeBy, 
+        sizeBy: o.series.sizeBy,
         lineWidth: o.series.lineWidth,
         states: o.series.states,
         marker: o.series.marker
 
+    };
+
+    this.bubble = {
+        tooltip: {
+            followPointer: true
+        }
     };
 
 }
--- scripts_src/constructors/charts/tooltip.js
+++ scripts_src/constructors/charts/tooltip.js
@@ -1,18 +1,18 @@
 /** Tooltip options constructor. Info at http://api.highcharts.com/highcharts#tooltip
-* @constructor Tooltip 
-* @param o {object} Tooltip options object
-*/
+ * @constructor Tooltip 
+ * @param o {object} Tooltip options object
+ */
 
 var Tooltip = function (o) {
     this.backgroundColor = '#FEFFEF';
     this.crosshairs = o.crosshairs || [false, false];
     this.formatter = o.formatter;
-    
+
     this.style = {
         color: '#000000',
         fontFamily: 'Calibri, Verdana, Arial, Helvetica, sans-serif'
     };
-    
+
     this.useHTML = false;
 
 }
