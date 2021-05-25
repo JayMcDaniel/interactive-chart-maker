1f1154cb4b3d260757aee66fb490cdb0b67d8083
Jay McDaniel <jaymcdan@gmail.com>
2021-05-25 13:15:23 -0700

    new computer

--- scripts_src/app.js
+++ scripts_src/app.js
@@ -48,5 +48,5 @@ $(function() {
   });
 
   //set app version (better through JS than statically to make sure JS is new)
-  $("#app_version").text("v2.12.0"); 
+  $("#app_version").text("v2.12.1"); 
 });
